a5907afac27a1e371e2d60b9d9f76151d982c42c
MichaelMeissner <github@the-meissners.org>
2025-11-21 18:35:08 -0500

    Incorporate Chris.nz's changes from November 2nd, 2025

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,13 @@
 // The index of the currently selected eye definitions
 static uint32_t defIndex{0};
 
+#ifdef ORIG_CODE
+TwoWire WIRE = Wire2;
+#else
+TwoWire WIRE = Wire;
+#endif
 LightSensor lightSensor(LIGHT_PIN);
-PersonSensor personSensor(Wire);
+PersonSensor personSensor(WIRE);
 bool personSensorFound = USE_PERSON_SENSOR;
 
 bool hasBlinkButton() {
@@ -55,7 +60,7 @@ void setup() {
   }
 
   if (hasPersonSensor()) {
-    Wire.begin();
+    WIRE.begin();
     personSensorFound = personSensor.isPresent();
     if (personSensorFound) {
       Serial.println("Person Sensor detected");
